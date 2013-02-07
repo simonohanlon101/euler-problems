c8d33523b9e6475e0d6138d561205b410c06daa3
Simon O'Hanlon <simon.ohanlon@gmail.com>
2013-02-07 21:47:02 +0000

    Update Problem 2.R

--- Problem 2.R
+++ Problem 2.R
@@ -12,15 +12,15 @@
 
 
 even_fib <- function( lim ){
-  a <- 1
-	b <- 1
-	ans <- 0
-	while( b < lim ){
-		c <- a + b
+    a <- 1
+    b <- 1
+    ans <- 0
+    while( b < lim ){
+        c <- a + b
 		a <- b
 	    b <- c
 		if( c %% 2 == 0 ){
-			ans <- ans + c
+		    ans <- ans + c
 		}	
 	}
 	return( paste( "The sum of even Fibonnaci numbers below " , lim , " is " , ans , sep = "" ) )
