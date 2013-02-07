0f372de0c0413c79d0750ecb35369f07f05b44a8
Simon O'Hanlon <simon.ohanlon@gmail.com>
2013-02-07 13:45:01 -0800

    Project Euler Problem 2 code

---
+++ Problem 2.R
@@ -0,0 +1,32 @@
+#
+#
+# Project Euler  	// Problem 2:
+# What is the sum of all even Fibonacci numbers below 4e6?
+# 
+#
+
+
+
+# Function to generate Fibonacci numbers up to some limit and sum the even numbers in that sequence
+# Running time for numbers up to 4 million is ~ 5 to 10 milliseconds (0.005 to 0.001s)
+
+
+even_fib <- function( lim ){
+  a <- 1
+	b <- 1
+	ans <- 0
+	while( b < lim ){
+		c <- a + b
+		a <- b
+	    b <- c
+		if( c %% 2 == 0 ){
+			ans <- ans + c
+		}	
+	}
+	return( paste( "The sum of even Fibonnaci numbers below " , lim , " is " , ans , sep = "" ) )
+}
+
+# > even_fib( 4000000 )
+# [1] "The sum of even Fibonnaci numbers below 4e+06 is 4613732"
+
+
