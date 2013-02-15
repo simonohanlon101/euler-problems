1863dcd8744cfd7921b9e0fe28b1359903bc18d5
Simon O'Hanlon <simon.ohanlon@gmail.com>
2013-02-15 16:05:46 +0000

    Update Problem 6.R

--- Problem 6.R
+++ Problem 6.R
@@ -8,7 +8,7 @@
 # Problem blurb:
 # ==============================================
 # The sum of the squares of the first ten natural numbers is,
-# 12 + 22 + ... + 102 = 385
+# 1^2 + 2^2 + ... + 10^2 = 385
 #
 # The square of the sum of the first ten natural numbers is,
 # (1 + 2 + ... + 10)^2 = 55^2 = 3025
